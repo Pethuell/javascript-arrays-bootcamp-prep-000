81a36df6f1d29c3a8cd3457e37d20343f9662380
paul@paulleach.co.uk <paul@paulleach.co.uk>
2018-07-30 19:40:02 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,9 +1,39 @@
 var chocolateBars = ['snickers','hundred grand','kitkat','skittles'];
 
-function addElementToBeginningOfArray(){
-  
+function addElementToBeginningOfArray(arrOrig, plusOne){
+  return [plusOne,...arrOrig];
 }
 
-function destructivelyAddElementToBeginningOfArray() {
-  
+function destructivelyAddElementToBeginningOfArray(arrOrig, plusOne) {
+  arrOrig.unshift(plusOne);
+  return arrOrig;
+}
+
+function addElementToEndOfArray(arrOrig, plusOne) {
+  return [...arrOrig, plusOne];
+}
+
+function destructivelyAddElementToEndOfArray(arrOrig, plusOne) {
+  arrOrig.push(plusOne);
+  return arrOrig;
+}
+
+function accessElementInArray(arrOrig, indexNo) {
+  return arrOrig[indexNo];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(arrOrig) {
+  return arrOrig.shift();
+}
+
+function removeElementFromBeginningOfArray(arrOrig) {
+  return arrOrig.slice(1);
+}
+
+function destructivelyRemoveElementFromEndOfArray(arrOrig) {
+  return arrOrig.pop();
+}
+
+function removeElementFromEndOfArray(arrOrig) {
+  return arrOrig.slice(0 , arrOrig.length -1);
 }
